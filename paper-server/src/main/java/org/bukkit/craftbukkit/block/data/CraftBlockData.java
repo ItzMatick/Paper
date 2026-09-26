d43e293fbaf584b3ec88dceadc9829ec4bfe2f16
ItzMatick <martin11.kubicek@gmail.com>
2026-09-26 21:51:30 +0200

    Removed getInstrument method from BlockData.

--- paper-server/src/main/java/org/bukkit/craftbukkit/block/data/CraftBlockData.java
+++ paper-server/src/main/java/org/bukkit/craftbukkit/block/data/CraftBlockData.java
@@ -30,12 +30,10 @@ import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateHolder;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
-import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.apache.commons.lang3.mutable.MutableDouble;
 import org.bukkit.Color;
-import org.bukkit.Instrument;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.SoundGroup;
@@ -657,11 +655,5 @@ public class CraftBlockData implements BlockData {
     @Override
     public boolean isReplaceable() {
         return this.state.canBeReplaced();
-    }
-
-    @Override
-    public Instrument getInstrument() {
-        NoteBlockInstrument noteBlockInstrument = this.state.instrument();
-        return fromVanilla(noteBlockInstrument, Instrument.class);
     }
 }
